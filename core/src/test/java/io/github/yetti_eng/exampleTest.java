338f3e8bb529f0ccade22d590b818e7706ddb50b
j-last <jacoblast.jbl@gmail.com>
2025-11-20 11:53:53 +0000

    adding testing directory

---
+++ core/src/test/java/io/github/yetti_eng/exampleTest.java
@@ -0,0 +1,12 @@
+package io.github.gamename;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ExampleTest {
+
+    @Test
+    public void exampleTest() {
+        assertTrue(true, "Dummy test");
+    }
+}
