4860ab9b02f8f36ec5366c40093c33ce4fd657f3
j-last <jacoblast.jbl@gmail.com>
2025-11-24 16:29:54 +0000

    Added a few more tests for YettiGame.java

--- core/src/test/java/io/github/yetti_eng/YettiGameTest.java
+++ core/src/test/java/io/github/yetti_eng/YettiGameTest.java
@@ -1,19 +1,74 @@
 package io.github.yetti_eng;
 
 import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class YettiGameTest {
 
     @Test
     public void scaledTest() {
+        // Given
+        float[] inputs = {3f, -3f, 0f, 1.5f};
         float worldScale = 80;
-        float[] testValues = {3f, -3f, 0f, 1.5f};
-
-        for (float testValue : testValues) {
-            float expected = testValue * worldScale;
-            float result = YettiGame.scaled(testValue);
-            assertEquals(expected, result, 0.001f);
+        
+        for (float input : inputs) {
+            // When
+            float expected = input * worldScale;
+            float result = YettiGame.scaled(input);
+            // Then
+            assertEquals(expected, result, 0.001f, "failed on input = " + input);
         }
+    }
+    
+    @Test
+    public void viewPortScaleTest() {
+    }
+
+    // Will need mockito for this I think
+    @Test
+    public void createDisposeTest() {
+    }
+
+    @Test
+    public void pauseTest() {
+    }
+
+    @Test
+    public void resumeTest() {
+    }
+
+    @Test
+    public void pauseAndResumeTest() {
+    }
+
+    @Test
+    public void pauseAndResumeMultipleTimesTest() {
+    }
+
+    @Test
+    public void calculateFinalScoreNormalTest() {
+        // Given
+        YettiGame Game = new YettiGame();
+        Game.score = 50;
+        Game.timer = new Timer(250);
+        // When
+        Game.calculateFinalScore();
+        int result = Game.score;
+        // Then
+        assertEquals(300, result, "Incorrectly calculated score when score = 50 & timer = 250");
+    }
+
+    @Test
+    public void calculateFinalScoreZeroTest() {
+        // Given
+        YettiGame Game = new YettiGame();
+        Game.score = 0;
+        Game.timer = new Timer(0);
+        // When
+        Game.calculateFinalScore();
+        int result = Game.score;
+        // Then
+        assertEquals(0, result, "Incorrectly calculated score when score = 0 & timer = 0");
     }
 }
