9b984792384a17773b1a26ec7011f89bf65512d6
j-last <jacoblast.jbl@gmail.com>
2025-11-26 12:22:12 +0000

    'YettiGame tests mostly done (but need to set up Mockito before doing a few more) & 'Timer' tests started

---
+++ core/src/test/java/io/github/yetti_eng/TimerTest.java
@@ -0,0 +1,37 @@
+package io.github.yetti_eng;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TimerTest {
+    
+    @Test
+    public void createTimerTest() {
+        Timer timer = new Timer(300);
+
+        int time_remaining = timer.getRemainingTime();
+
+        assertEquals(300, time_remaining, "Timer has the incorrect duration when created");
+        assertFalse(timer.isActive(), "Timer shouldn't be active when created");
+        assertFalse(timer.isFinished(), "Timer shouldn't be finished when created");
+    }
+
+    @Test
+    public void createTimerZeroTest() {
+        Timer timer = new Timer(0);
+
+        int time_remaining = timer.getRemainingTime();
+
+        assertEquals(0, time_remaining);
+        assertFalse(timer.isActive(), "Timer shouldn't be active when created");
+        assertFalse(timer.isFinished(), "Timer shouldn't be finished when created");
+    }
+
+    @Test
+    public void createNegativeTimerTest() {
+        assertThrows(AssertionError.class, () -> new Timer(-10));
+    }
+
+    
+
+}
--- core/src/test/java/io/github/yetti_eng/YettiGameTest.java
+++ core/src/test/java/io/github/yetti_eng/YettiGameTest.java
@@ -1,8 +1,10 @@
 package io.github.yetti_eng;
 
 import org.junit.jupiter.api.*;
-
 import static org.junit.jupiter.api.Assertions.*;
+
+//import org.junit.jupiter.api.BeforeEach;
+//import static org.mockito.Mockito.mock;
 
 public class YettiGameTest {
 
@@ -20,30 +22,52 @@ public class YettiGameTest {
             assertEquals(expected, result, 0.001f, "failed on input = " + input);
         }
     }
-    
-    @Test
-    public void viewPortScaleTest() {
-    }
+    // Needds Mockito
+    //@Test
+    //public void viewPortScaleTest() {
+    //    YettiGame Game = new YettiGame();
+    //    Game.create();
+    //    System.out.print(Game.viewport.getScreenHeight());
+    //    Game.viewport.getScreenHeight();
+    //}
 
     // Will need mockito for this I think
     @Test
     public void createDisposeTest() {
+
     }
 
     @Test
     public void pauseTest() {
+        YettiGame Game = new YettiGame();
+
+        Game.pause();
+
+        assertTrue(Game.isPaused());
     }
 
     @Test
     public void resumeTest() {
+        YettiGame Game = new YettiGame();
+
+        Game.pause();
+        assertTrue(Game.isPaused());
+
+        Game.resume();
+        assertTrue(!Game.isPaused());
     }
 
     @Test
-    public void pauseAndResumeTest() {
-    }
+    public void multiplePauseAndResumeTest() {
+        YettiGame Game = new YettiGame();
 
-    @Test
-    public void pauseAndResumeMultipleTimesTest() {
+        for (int i = 0; i < 10; i++) {
+            Game.pause();
+            assertTrue(Game.isPaused());
+
+            Game.resume();
+            assertTrue(!Game.isPaused());
+        }
     }
 
     @Test
