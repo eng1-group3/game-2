58d869e0faf8e2b4bc7f5c55c7af95d65812ad9d
lily <lilygaunt8@gmail.com>
2025-11-27 18:50:57 +0000

    Fixed menu screen UI.

--- core/src/main/java/io/github/yetti_eng/screens/MenuScreen.java
+++ core/src/main/java/io/github/yetti_eng/screens/MenuScreen.java
@@ -2,9 +2,11 @@ package io.github.yetti_eng.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.utils.Align;
@@ -28,17 +30,16 @@ public class MenuScreen implements Screen {
     public void show() {
         Gdx.input.setInputProcessor(stage);
 
+        // make table fill whole window
         table.setFillParent(true);
-
-        table.setDebug(true);
+        //table.setDebug(true);
 
         stage.addActor(table);
 
         TextButton.TextButtonStyle style = new TextButton.TextButtonStyle(null, null, null, game.font);
+        Label.LabelStyle labelStyle = new Label.LabelStyle(game.font, Color.WHITE);
 
-        float centre_x = stage.getViewport().getWorldWidth() / 2;
-        float centre_y = stage.getViewport().getWorldHeight() / 2;
-        float height = stage.getViewport().getWorldHeight();
+        Label titleLabel = new Label("Welcome to YettiGame", labelStyle);
 
         TextButton playButton = new TextButton("Play", style);
         playButton.addListener(new InputListener() {
@@ -49,7 +50,6 @@ public class MenuScreen implements Screen {
                 return true;
             }
         });
-        table.add(playButton).pad(10).row();
 
         TextButton settingsButton = new TextButton("Settings", style);
         settingsButton.addListener(new InputListener() {
@@ -60,7 +60,6 @@ public class MenuScreen implements Screen {
                 return true;
             }
         });
-        table.add(settingsButton).row();
 
         TextButton creditsButton = new TextButton("Credits", style);
         creditsButton.addListener(new InputListener() {
@@ -71,7 +70,6 @@ public class MenuScreen implements Screen {
                 return true;
             }
         });
-        table.add(creditsButton).pad(10).row();
 
         TextButton quitButton = new TextButton("Quit", style);
         quitButton.addListener(new InputListener() {
@@ -81,26 +79,23 @@ public class MenuScreen implements Screen {
                 return true;
             }
         });
+
+        table.add(titleLabel).padBottom(20).row();
+        table.add(playButton).pad(10).row();
+        table.add(settingsButton).pad(10).row();
+        table.add(creditsButton).pad(10).row();
         table.add(quitButton).pad(10).row();
+
     }
 
     @Override
     public void render(float delta) {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        game.uiViewport.apply();
-        game.batch.setProjectionMatrix(game.uiViewport.getCamera().combined);
-        game.batch.begin();
-        game.font.draw(game.batch, "Welcome to YettiGame", 0, game.uiViewport.getWorldHeight()-50, game.uiViewport.getWorldWidth(), Align.center, false);
-        game.batch.end();
-
-        stage.getViewport().apply();
-        stage.act(delta);
         stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
-        game.uiViewport.update(width, height, true);
         stage.getViewport().update(width, height, true);
     }
 
