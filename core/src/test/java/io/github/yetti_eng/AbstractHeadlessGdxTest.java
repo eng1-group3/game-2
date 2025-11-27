ae2a0cb5da2698424e4dc663dc607b5cb8678a08
j-last <jacoblast.jbl@gmail.com>
2025-11-27 16:43:41 +0000

    added comments to AbstractHeadlessGdxTest.java

--- core/src/test/java/io/github/yetti_eng/AbstractHeadlessGdxTest.java
+++ core/src/test/java/io/github/yetti_eng/AbstractHeadlessGdxTest.java
@@ -1,7 +1,11 @@
 package io.github.yetti_eng;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+
+// EXTEND THIS CLASS IF DOING LIBGDX SPECIFIC TESTS
+// The commented lines are needed for Mockito (but I couldn't get it to work in our project because I am stupid)
+
+//import com.badlogic.gdx.Gdx;
+//import com.badlogic.gdx.graphics.GL20;
 import org.junit.jupiter.api.BeforeEach;
  
 //import static org.mockito.Mockito.mock;
