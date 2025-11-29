587bb16752514b4175915b9a95c70489698cc1a6
taro-chandiwana <tchandiwana@outlook.com>
2025-11-29 11:36:03 +0000

    adds scores tied with last place of leaderboard

--- core/src/main/java/io/github/yetti_eng/Leaderboard.java
+++ core/src/main/java/io/github/yetti_eng/Leaderboard.java
@@ -1,15 +1,13 @@
 package io.github.yetti_eng;
 import java.io.FileWriter;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Scanner;
+import java.util.*;
 import java.io.IOException;
-import java.io.File;                  // Import the File class
+import java.io.File;
 import java.io.FileNotFoundException;
 
 public class Leaderboard {
     private int leaderboardSize = 5;
+    private List topScores = new ArrayList();
 
     public void addToLeaderboard(String playerName, int score) {
         try (FileWriter myWriter = new FileWriter("leaderboard.txt", true)) {
@@ -23,41 +21,84 @@ public class Leaderboard {
     public void getTopScores(){
         File leaderboard = new File("leaderboard.txt");
 
-        try (Scanner myReader = new Scanner(leaderboard)) {
-            LeaderboardEntry topScores[] = new LeaderboardEntry[leaderboardSize];
-            Arrays.fill(topScores, new LeaderboardEntry());
+        try (Scanner scanner = new Scanner(leaderboard)) {
+            LeaderboardEntry lowestScore =  new LeaderboardEntry();
 
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
+            // Add leaderboardSize entries to the topScores arraylist
+            for (int i = 0; i < leaderboardSize; i++) {
+                topScores.add(new LeaderboardEntry("", 0));
+            }
 
-                int lowestScore = topScores[0].score;
-                int lowestScoreIndex = 0;
+            // Looks through each line of the leaderboard.txt file
+            while (scanner.hasNextLine()) {
+                String data = scanner.nextLine();
 
-                for (int i = 1; i < topScores.length; i++) {
-                    if (topScores[i].score < lowestScore) {
-                        lowestScoreIndex = i;
-                        lowestScore = topScores[i].score;
+                // Lowest score is to the first entry of the arraylist
+                // Goes through each element of the top scores and if the score is lower than the current lowest score,
+                // it becomes the new lowest score
+                lowestScore = (LeaderboardEntry) topScores.get(0);
+                for (Object object : topScores) {
+                    LeaderboardEntry entry = (LeaderboardEntry) object;
+                    if (entry.getScore() < lowestScore.getScore()) {
+                        lowestScore = entry;
                     }
                 }
 
-                int score = Integer.parseInt(data.substring(data.indexOf(" ") + 1));
+                // For current line in the leaderboard file, the first part is the name,
+                // and the second part is the score
                 String playerName = data.substring(0, data.indexOf(" "));
-                //System.out.println(playerName);
+                int score = Integer.parseInt(data.substring(data.indexOf(" ") + 1));
+                LeaderboardEntry leaderboardEntry = new LeaderboardEntry(playerName, score);
 
-                if(score > lowestScore){
-                    topScores[lowestScoreIndex] = new LeaderboardEntry(playerName, score);
+                // If the score of the current line is higher than the lowest score,
+                // the entry gets added to the topEntries list, replacing the lowest score
+                if(leaderboardEntry.getScore() > lowestScore.getScore()){
+                    topScores.add(leaderboardEntry);
+                    topScores.remove(lowestScore);
                 }
-                SortLeaderboard sortedLeaderboard = new SortLeaderboard();
-                Arrays.sort(topScores, sortedLeaderboard);
 
                 System.out.println("\nTop scores:");
-                for (LeaderboardEntry entry : topScores) {
-                    System.out.println(entry.playerName + " " + entry.score);
+                for (Object obj : topScores) {
+                    LeaderboardEntry e = (LeaderboardEntry) obj;
+                    e.print();
                 }
             }
+
+
+            try (Scanner scanner2 = new Scanner(leaderboard)) {
+                // Looks through each line of the leaderboard.txt file
+                while (scanner2.hasNextLine()) {
+                    String data = scanner2.nextLine();
+
+                    // Gets the playerName and score for the line of leaderboard.txt
+                    String playerName = data.substring(0, data.indexOf(" "));
+                    int score = Integer.parseInt(data.substring(data.indexOf(" ") + 1));
+                    LeaderboardEntry entry = new LeaderboardEntry(playerName, score);
+
+                    // Adds entry if the score is tied with last place of the leaderboard
+                    if (score == lowestScore.getScore() && !topScores.contains(entry)) {
+                        topScores.add(new LeaderboardEntry(playerName, score));
+                    }
+                }
+            }
+            catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            // Sorts topScores using comparators
+            Comparator myComparator = new SortLeaderboard();
+            Collections.sort(topScores, myComparator);
+
+            System.out.println("\nTop scores:");
+            for (Object obj : topScores) {
+                LeaderboardEntry e = (LeaderboardEntry) obj;
+                e.print();
+            }
+
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+
     }
 }
--- core/src/main/java/io/github/yetti_eng/LeaderboardEntry.java
+++ core/src/main/java/io/github/yetti_eng/LeaderboardEntry.java
@@ -1,18 +1,51 @@
 package io.github.yetti_eng;
 import java.util.Comparator;
 
-public class LeaderboardEntry{
-    public String playerName;
-    public int score;
+public class LeaderboardEntry {
+    private String playerName;
+    private int score;
 
-    public LeaderboardEntry(String playerName, int score){
+    public LeaderboardEntry(String playerName, int score) {
         this.playerName = playerName;
         this.score = score;
     }
 
-    public LeaderboardEntry(){
-        this.playerName = "";
+    public LeaderboardEntry() {
+        this.playerName = null;
         this.score = Integer.MIN_VALUE;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void print() {
+        System.out.println(this.score + " " + this.playerName);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        LeaderboardEntry other = (LeaderboardEntry) obj;
+        if (this == other) {
+            return true;
+        }
+
+        if (this.getPlayerName().equals(other.getPlayerName()) && this.getScore() == other.getScore()) {
+            return true;
+        }
+        return false;
     }
 }
 
@@ -20,7 +53,7 @@ class SortLeaderboard implements Comparator<LeaderboardEntry> {
 
     @Override
     public int compare(LeaderboardEntry o1, LeaderboardEntry o2) {
-        return Integer.compare(o1.score, o2.score);
+        return Integer.compare(o1.getScore(), o2.getScore());
     }
 }
 
