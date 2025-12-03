9887ca08ab6f0c00f4fd005a8006e8cc9e79194b
j-last <jacoblast.jbl@gmail.com>
2025-12-03 16:31:29 +0000

    Entity unit tests and some other tweaks

---
+++ core/src/test/java/io/github/yetti_eng/entities/EntityTestable.java
@@ -0,0 +1,9 @@
+package io.github.yetti_eng.entities;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class EntityTestable extends Entity {
+    public EntityTestable(Texture tex, float x, float y, float width, float height, float speed, boolean isSolid) {
+        super(tex, x, y, width, height, speed, isSolid);
+    }
+}
